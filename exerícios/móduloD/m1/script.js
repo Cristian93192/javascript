f40a5308928320bdc921ae2169ea36bb1db32cde
Cristian Alves <ruanc8070@gmail.com>
2025-11-07 21:00:59 -0300

    exercicios/MóduloD

---
+++ exerícios/móduloD/m1/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imgdia')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'man.jpg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE!
+        img.src = 'tarde.jpg'
+        document.body.style.background = '#b9846f'
+    } else {
+        //BOA NOITE!
+        img.src = 'noite.jpg'
+        document.body.style.background = '#515154'
+    }
+}
