1f19887748147fca9dc142a2904a78b48a9987ed
Cristian Alves <ruanc8070@gmail.com>
2025-11-09 17:57:40 -0300

    ex02/móduloD

---
+++ exerícios/móduloD/m2/script.js
@@ -0,0 +1,56 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO]Verifique os dados e tente novamente!')
+    } else {
+
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+
+            if (idade >= 0 && idade < 10) {
+                //CRIANÇA
+                img.setAttribute('src', 'cria-m.jpg')
+            } else if (idade < 21) {
+                //JOVEM
+                img.setAttribute('src', 'jovem-m.jpg')
+            } else if (idade < 50) {
+                //ADULTO
+                img.setAttribute('src', 'jovem-m.jpg')
+            } else {
+                //IDOSO
+                img.setAttribute('src', 'idoso-m.jpg')
+            }
+
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+
+            if (idade >= 0 && idade < 10) {
+                //CRIANÇA
+                img.setAttribute('src', 'cria-f.jpg')
+            } else if (idade < 21) {
+                //JOVEM
+                img.setAttribute('src', 'jovem-f.jpg')
+            } else if (idade < 50) {
+                //ADULTO
+                img.setAttribute('src', 'jovem-f.jpg')
+            } else {
+                //IDOSO
+                img.setAttribute('src', 'idoso-f.jpg')
+            }
+
+        }
+        
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
