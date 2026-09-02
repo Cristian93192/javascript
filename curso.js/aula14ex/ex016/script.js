c67102a133a63013da7fa440f7c2a9422dee6d42
Cristian Alves <ruanc8070@gmail.com>
2026-09-02 20:07:42 -0300

    ex016-solução

--- curso.js/aula14ex/ex016/script.js
+++ curso.js/aula14ex/ex016/script.js
@@ -1,18 +1,34 @@
-function verificar(){
-    var start = document.getElementById('n1')
-    var end = document.getElementById('n2')
-    var step = document.getElementById('passo')
-}
+function contar() {
+    var ini = document.getElementById('ni')
+    var fim = document.getElementById('nf')
+    var passo = document.getElementById('np')
+    var res = document.getElementById('res')
 
-if (start < 1) {
-    console.log('Impossivel de executar')
-} else if (end <= start) {
-    console.log('Impossível de executar')
-} else if (step < 1) {
-    console.log('Impossível de executar')
-}
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar'
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number (passo.value)
+        if (p<= 0){
+            window.alert('Passo inválido!Considerando passo 1')
+            p=1
+        }
 
-while(end<=start) {
-    console.log('start')
-    start = start + step
+        if(i < f) {
+            //Contagem crescente
+            for(var c = i; c <= f; c+=p) {
+                res.innerHTML += `${c} \u{1f449}`
+            }
+
+        } else {
+            //Contagem decrescente
+            for(var c = i; c>= f; c-=p) {
+                res.innerHTML += `${c} \u{1f449}`
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
+
+    }
 }
